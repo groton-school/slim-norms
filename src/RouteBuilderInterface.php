bf52e5c975ca577006b7f44cafcdd14464c1b9b3
Seth Battis <sbattis@groton.org>
2025-09-18 20:59:18 -0400

    feat: optional inner middleware inserted into routes

--- src/RouteBuilderInterface.php
+++ src/RouteBuilderInterface.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace GrotonSchool\Slim\Norms;
 
+use Psr\Http\Server\MiddlewareInterface;
 use Slim\App;
 use Slim\Interfaces\RouteGroupInterface;
 
 interface RouteBuilderInterface
 {
-    public function define(App $app): RouteGroupInterface;
+    public function define(App $app, ?MiddlewareInterface ...$innerMiddleware): RouteGroupInterface;
 }
